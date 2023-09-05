4c16c78d5cabb8ce1e4e212a0917fcd5b36c491c
BASISTK <ssalanci70@gmail.com>
2023-09-05 10:45:11 +0200

    daco

--- scss/script.js
+++ scss/script.js
@@ -12,3 +12,23 @@ function disableScroll() {
 function enableScroll() {
     window.onscroll = function() {};
 }
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect.top;
+        var revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
